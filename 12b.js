d0371798087b6a93ee493cacd4aa15ed44850488
Marek Ulicny <ulicnym@magexo.cz>
2022-12-12 20:37:07 +0100

    add day 12

---
+++ 12b.js
@@ -0,0 +1,72 @@
+import { readLines } from './helpers.js'
+
+// Functions
+
+const findCharPosition = (data, char) => {
+  for (let i = 0; i < data.length; i++) {
+    for (let j = 0; j < data[i].length; j++) {
+      if (data[i][j] === char) return [i, j]
+    }
+  }
+}
+
+const isTooHigh = (a, b) => b.charCodeAt(0) - a.charCodeAt(0) > 1
+
+const canWalk = (cur, next) => {
+  if (!cur || !next) return false
+
+  const curElev = map[cur[0]]?.[cur[1]]
+  let nextElev = map[next[0]]?.[next[1]]
+
+  if (!nextElev) return false
+  if (nextElev === 'E') nextElev = 'z' // replace end
+
+  const nextTrack = track[next[0]][next[1]]
+
+  if (nextTrack !== '.') return false
+
+  return !isTooHigh(nextElev, curElev)
+}
+
+const walk = (y, x, step) => {
+  console.log(y, x, step)
+
+  track[y][x] = step
+
+  if (canWalk([y, x], [y, x - 1])) queue.push([y, x - 1, step + 1])
+  if (canWalk([y, x], [y, x + 1])) queue.push([y, x + 1, step + 1])
+  if (canWalk([y, x], [y - 1, x])) queue.push([y - 1, x, step + 1])
+  if (canWalk([y, x], [y + 1, x])) queue.push([y + 1, x, step + 1])
+}
+
+const main = () => {
+  const [y, x] = findCharPosition(map, 'E')
+
+  // replace start
+  map[y] = map[y].replace('E', 'z')
+
+  queue.push([y, x, 0])
+
+  while (queue.length > 0) {
+    const [y, x, step] = queue.shift()
+
+    if (track[y][x] !== '.') continue
+
+    if (map[y][x] === 'a') {
+      console.log('step:', step)
+      break
+    }
+
+    walk(y, x, step)
+  }
+}
+
+// Main
+
+const map = readLines('./12.txt')
+const width = map[0].length
+const height = map.length
+const track = [...Array(height)].map(() => Array(width).fill('.'))
+const queue = []
+
+main()
