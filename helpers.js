e72038ce63055065032d0a37e9acbde8a131872a
Marek Ulicny <ulicnym@magexo.cz>
2022-12-12 17:39:12 +0100

    update tooling

--- helpers.js
+++ helpers.js
@@ -1,3 +1,9 @@
 import { readFileSync } from 'node:fs'
 
-export const readLines = (path) => readFileSync(path).toString().split('\n')
+export const readLines = (path) => {
+  const lines = readFileSync(path).toString().split('\n')
+
+  if (lines[lines.length - 1] === '') lines.pop()
+
+  return lines
+}
